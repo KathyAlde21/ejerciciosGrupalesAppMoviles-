b8c99ccdec06f43f3c7aa19e203308918c5da662
Katherine Alderete Gomez <katherinealdereteg@gmail.com>
2024-09-28 19:53:33 -0300

    actualizando

---
+++ src/ejercicioGrupal12/EjercicioGrupal12.java
@@ -0,0 +1,14 @@
+
+package ejercicioGrupal12;
+
+
+public class EjercicioGrupal12 {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        
+        
+        
+        
+    }
+    
+}
